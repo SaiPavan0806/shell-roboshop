677da1ec11a4734d12c91c542fb7376bbede7ad3
Bharath <bharathvaliveti06@gmail.com>
2025-10-02 21:50:09 -0700

    message

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f" #AMI ID
 SG_ID="sg-0c0a10aa0c127c8db" #security Group ID
+ZONE_ID="Z0531895FHVQAQ3TAJWF" #Domain Zone ID we get this from Route 53
+DOMAIN_NAME="daws86s.online"
 
 for instance in $@
 do
@@ -10,9 +12,28 @@ do
     #Get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" #Mongodb.daws86s.online
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" #daws86s.online
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '{
+    "Changes": [{
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "$RECORD_NAME",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [{
+          "Value": "$IP"
+        }]
+      }
+    }]
+  }'
+
 done
